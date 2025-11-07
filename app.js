2b2f701ff8c650c011a121d45b2fe827a71f0e85
Deepthi-M555 <mdeepthi555@gmail.com>
2025-11-07 16:50:39 +0530

    imported requirements

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.json());
